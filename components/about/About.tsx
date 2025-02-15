502247a0a0ae41a7d1432b30433f76a90cce58ca
Sophia See <sophiasee11@gmail.com>
2025-02-15 20:29:41 +0800

    added patterns in about section

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -10,21 +10,31 @@ import Item from './Item'
 import ItemContainer from './ItemContainer'
 import useDeviceSize from '@/hooks/use-device-size'
 
+function ImageContainer ({children}: {children:React.ReactNode}) {
+  return (
+    <div className='relative h-[400px] md:h-[360px]'>
+      {children}
+    </div>
+  )
+}
+
 export default function About() {
   const {currSize} = useDeviceSize();
 
   return (
-    <section className='pb-[101px]'>
-      <div className="relative h-[662px] md:h-[675px] md:mb-[118px]">
+    <section>
+      <div className="relative h-[662px] md:h-[675px]">
         <AboutItem className="absolute top-[-72px] md:top-[-96px]">
-          <Image
-            src={`/images/homepage/enjoyable-place-${currSize}.jpg`}
-            sizes="100vw"
-            width={0}
-            height={0}
-            alt="a place full of nature"
-            className="w-full h-[400px] shadow-[0_75px_100px_-50px_rgba(56,66,85,50%)]"
-          />
+          <ImageContainer>
+            <Image
+              src={`/images/homepage/enjoyable-place-${currSize}.jpg`}
+              sizes="100vw"
+              width={0}
+              height={0}
+              alt="a place full of nature"
+              className="w-full h-full shadow-[0_75px_100px_-50px_rgba(56,66,85,50%)]"
+            />
+          </ImageContainer>
           <ItemContainer>
             <Divider />
             <Item>
@@ -37,17 +47,39 @@ export default function About() {
               />
             </Item>
           </ItemContainer>   
-        </AboutItem>   
+        </AboutItem>
+        <div className='hidden md:block absolute top-[50%] right-[50%] -translate-y-[70%]'>
+          <Image
+            src={"/images/patterns/pattern-curve-top-right.svg"}
+            sizes='100vw'
+            width={0}
+            height={0}
+            alt='pattern with curved at the top right'
+            className='w-auto h-[320px]'
+          />
+        </div>
       </div>
-      <AboutItem>
-        <Image
-          src={`/images/homepage/locally-sourced-${currSize}.jpg`}
-          sizes="100vw"
-          width={0}
-          height={0}
-          alt="a freshly cooked food"
-          className="w-full h-[400px] shadow-[0_75px_100px_-50px_rgba(56,66,85,50%)]"
-        />
+      <AboutItem className='relative h-full md:pb-[118px]'>
+        <ImageContainer>
+          <Image
+            src={`/images/homepage/locally-sourced-${currSize}.jpg`}
+            sizes="100vw"
+            width={0}
+            height={0}
+            alt="a freshly cooked food"
+            className="w-full h-full shadow-[0_75px_100px_-50px_rgba(56,66,85,50%)]"
+          />
+          <div className='absolute top-[70%] right-[-10%]'>
+            <Image
+              src={"/images/patterns/pattern-lines.svg"}
+              sizes='100vw'
+              width={0}
+              height={0}
+              className='w-auto h-auto'
+              alt='lines pattern'
+            />
+          </div>
+        </ImageContainer>
         <ItemContainer>
           <Divider />
           <Item>
@@ -59,7 +91,17 @@ export default function About() {
               text="All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food."
             />
           </Item>
-        </ItemContainer>   
+        </ItemContainer>
+        <div className='hidden md:block absolute bottom-0 -right-[67%] overflow-hidden'>
+          <Image
+            src={"/images/patterns/pattern-curve-top-left.svg"}
+            sizes='100vw'
+            width={0}
+            height={0}
+            alt='pattern with curved at the top right'
+            className='w-auto h-[320px]'
+          />
+        </div>
       </AboutItem> 
     </section>
   )
--- components/about/ItemContainer.tsx
+++ components/about/ItemContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ItemContainer({children}: {children: React.ReactNode}) {
   return (
-    <div className="flex flex-col justify-center items-center gap-9 md:gap-[39px]">
+    <div className="flex flex-col justify-center items-center gap-9 md:gap-[39px] md:mx-[58px]">
       {children}
     </div>
   )
